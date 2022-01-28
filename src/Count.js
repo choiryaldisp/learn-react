a7f709c74b1c75d939f27d8f3698d8c845caeaca
choiryaldisp8 <choiryaldisp8@gmail.com>
2022-01-28 13:46:18 +0700

    [EXERCISE] Learn to using arrow function

--- src/Count.js
+++ src/Count.js
@@ -8,14 +8,36 @@ class Count extends Component{
         }
     }
 
-    increament(){
+    // increament(){
+    //     // set state berguna untuk melakukan memanipulasi state
+    //     // set state maka akan terjadi re-render component
+    //     console.log('increament', this);
+    //     this.setState({ 
+    //         number : this.state.number +1
+    //     })
+    // }
+
+    increament=()=>{
         // set state berguna untuk melakukan memanipulasi state
         // set state maka akan terjadi re-render component
+        console.log('increament', this);
         this.setState({ 
-            number : this.state.number +1
+            number : this.state.number + 1
         })
     }
 
+    decreament(){
+        console.log('decreament', this);
+        if (this.state.number>0){
+            this.setState({
+                number : this.state.number -1
+            })
+        }
+    }
+
+    // Ketika menggunakan function biasa, 
+    // kita menggunakan binding untuk mengikat this yang kita kirimkan menjadi this function tersebut
+    // ketika kita menggunakn arrow function akan mengambil this dari induknya 
     render(){
         console.log('count', this);
         return(
@@ -30,7 +52,18 @@ class Count extends Component{
 
 
                 */}
-                <button onClick={this.increament.bind(this)}>+</button>
+                {/* <button onClick={this.increament.bind(this)}>+</button> */}
+                {/* Jika ingin memberikan parameter pada function maka berikan arrow function didepannya terlebih dahulu 
+                    Contoh: {()=> this.function(param)}
+                */}
+
+                {/* <button onClick={this.increament()}>+</button> ==> mengeksekusi function pada saat itu juga atau pada saat render pertama,
+                    sedangkan, 
+                    <button onClick={this.increament()}>+</button> ==> akan mengeksekusi pada saat re-rendering atau pada saat melakukan event
+                */}
+
+                <button onClick={this.increament}>+</button>
+                <button onClick={this.decreament.bind(this)}>-</button>
             </div>
         )
     }
